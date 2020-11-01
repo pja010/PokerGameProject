ec3b553150fa54b5c2663f57aa4b4ca2ddaf3a63
Callie Valenti <cgv004@bucknell.edu>
2020-11-01 16:08:51 -0500

    Initializing new project

---
+++ README.md
@@ -0,0 +1,19 @@
+# CSCI 205 - Software Engineering and Design
+Bucknell University
+Lewisburg, PA
+
+CSCI 205 – Software Engineering and Design Fall 2020
+
+Teamwork with Gitlab and IntelliJ 3
+### Course Info
+Instructor: Brian King
+Semester: Fall 2020
+## Team Information
+Callie Valenti
+Lindsay Knupp
+Per Astrom
+Guillermo Torres
+## Project Information
+Final Project- Poker Game
+## How to run it
+*Update this with information about how to build and run your project*
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2020
+ * Instructor: Prof. Brian King *
+ * Name: Callie Valenti
+ * Section: 11:30
+ * Date: 11/1/20
+ * Time: 3:57 PM
+ *
+ * Project: csci205FinalProject
+ * Package: main * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("Hello, World");
+    }
+}
+    
