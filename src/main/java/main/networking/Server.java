44a1b1bed629f9842506faf4fbe1668d658bdbf7
Lindsay Knupp <lak030@bucknell.edu>
2020-11-06 11:59:57 -0500

    Created networking package

---
+++ src/main/java/main/networking/Server.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2020
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/6/2020
+ * Time: 11:59 AM
+ *
+ * Project: csci205FinalProject
+ * Package: main.networking
+ * Class: Server
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main.networking;
+
+public class Server {
+}
