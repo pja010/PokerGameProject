19d3396204fea003ce96c94c86bb94ca21df7ead
Guillermo Torres <glt005@bucknell.edu>
2020-11-13 12:36:00 -0500

    GUI

--- src/main/java/main/pokergamemvc/PokerGameController.java
+++ src/main/java/main/pokergamemvc/PokerGameController.java
@@ -18,5 +18,48 @@
  */
 package main.pokergamemvc;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class PokerGameController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonBet;
+
+    @FXML
+    private Button buttonCheck;
+
+    // The model for this view
+    private PokerGameModel theModel;
+
+    @FXML
+    void onMouseClickedBet(MouseEvent event) {
+        System.out.println("Bet clicked");
+
+    }
+
+    @FXML
+    void onMouseClickedCheck(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert buttonBet != null : "fx:id=\"buttonBet\" was not injected: check your FXML file 'PokerGameView.fxml'.";
+        assert buttonCheck != null : "fx:id=\"buttonCheck\" was not injected: check your FXML file 'PokerGameView.fxml'.";
+
+    }
+
+    void setModel(PokerGameModel theModel) {
+        this.theModel = theModel;
+    }
 }
