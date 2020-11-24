fff5807156433d358ebff8b52071890e8deb9b46
Lindsay Knupp <lak030@bucknell.edu>
2020-11-24 11:45:59 -0500

    Created "Test" directory and ChipsTest class

---
+++ src/test/java/main/ChipsTest.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChipsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
