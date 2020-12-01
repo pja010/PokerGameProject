fa7b46d185e818ca620a953984c50e01d7b59d7d
pja010 <pja010@bucknell.edu>
2020-12-01 10:46:39 -0500

    Refactored ScoreUpdate.

--- src/main/java/main/ScoreUpdate.java
+++ src/main/java/main/ScoreUpdate.java
@@ -27,6 +27,27 @@ import java.util.Collections;
 public class ScoreUpdate implements Serializable {
 
     /**
+     * Scores for hands.
+     */
+    private final int SCORE_800 = 800;
+    private final int SCORE_700 = 700;
+    private final int SCORE_600 = 600;
+    private final int SCORE_400 = 400;
+    private final int SCORE_300 = 300;
+    private final int SCORE_200 = 200;
+    private final int SCORE_100 = 100;
+
+    /**
+     * Card hand positions.
+     */
+    private final int FIRST_CARD_POS = 0;
+    private final int SECOND_CARD_POSITION = 1;
+    private final int THIRD_CARD_POSITION = 2;
+    private final int FOURTH_CARD_POSITION = 3;
+    private final int FIFTH_CARD_POSITION = 4;
+    private final int SCORE_500 = 500;
+
+    /**
      * List containing the 5 cards constituting a player's hand.
      */
     private ArrayList<Integer> score;
@@ -39,48 +60,58 @@ public class ScoreUpdate implements Serializable {
     /**
      * Updates the score of the hand.
      * @param playerHand a list of a player's 5 cards.
-     *///todo - shorten this method
+     */
     public ScoreUpdate(ArrayList<Card> playerHand) {
         this.score = new ArrayList<>(PLAYER_HAND_SIZE);
         setInitialHand(score);
 
-        for (int i = 0; i < playerHand.size(); i++){
-            for (int j = i+1; j < playerHand.size(); j++) {
-                for (int k = j+1; k < playerHand.size(); k++){
-                    for (int p = k+1; p < playerHand.size(); p++) {
-                        for (int q = p+1; q < playerHand.size(); q++) {
+        for (int i = FIRST_CARD_POS; i < playerHand.size(); i++){
+            for (int j = i+SECOND_CARD_POSITION; j < playerHand.size(); j++) {
+                for (int k = j+SECOND_CARD_POSITION; k < playerHand.size(); k++){
+                    for (int p = k+SECOND_CARD_POSITION; p < playerHand.size(); p++) {
+                        for (int q = p+SECOND_CARD_POSITION; q < playerHand.size(); q++) {
                             Card card1 = playerHand.get(i);
                             Card card2 = playerHand.get(j);
                             Card card3 = playerHand.get(k);
                             Card card4 = playerHand.get(p);
                             Card card5 = playerHand.get(q);
-                            ArrayList<Integer> tempScore = new ArrayList<>(evaluate(card1, card2, card3, card4, card5));
-                            if (tempScore.get(0) >= this.score.get(0)) {
-                                if (tempScore.get(0) > this.score.get(0)) {
-                                    this.score = tempScore;
-                                }
-                                else if (tempScore.get(1) >= this.score.get(1)){
-                                    if (tempScore.get(1) > this.score.get(1)) {
-                                        this.score = tempScore;
-                                    }
-                                    else if (tempScore.get(2) >= this.score.get(2)){
-                                        if (tempScore.get(2) > this.score.get(2)){
-                                            this.score = tempScore;
-                                        }
-                                        else if(tempScore.get(3) >= this.score.get(3)){
-                                            if (tempScore.get(3) > this.score.get(3)){
-                                                this.score = tempScore;
-                                            }
-                                            else if(tempScore.get(4) >= this.score.get(4)){
-                                                if (tempScore.get(4) > this.score.get(4)) {
-                                                    this.score = tempScore;
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
 
+                            ArrayList<Integer> tempScore = new ArrayList<>(evaluate(card1, card2, card3, card4, card5));
+                            updatePlayerScoreFromTempScore(tempScore);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void updatePlayerScoreFromTempScore(ArrayList<Integer> tempScore) {
+        int firstCardScore = this.score.get(FIRST_CARD_POS);
+        int secondCardScore = this.score.get(SECOND_CARD_POSITION);
+        int thirdCardScore = this.score.get(THIRD_CARD_POSITION);
+        int fourthCardScore = this.score.get(FOURTH_CARD_POSITION);
+        int fifthCardScore = this.score.get(FIFTH_CARD_POSITION);
+
+        if (tempScore.get(FIRST_CARD_POS) >= firstCardScore) {
+            if (tempScore.get(FIRST_CARD_POS) > firstCardScore) {
+                this.score = tempScore;
+            }
+            else if (tempScore.get(SECOND_CARD_POSITION) >= secondCardScore) {
+                if (tempScore.get(SECOND_CARD_POSITION) > secondCardScore) {
+                    this.score = tempScore;
+                }
+                else if (tempScore.get(THIRD_CARD_POSITION) >= thirdCardScore) {
+                    if (tempScore.get(THIRD_CARD_POSITION) > thirdCardScore) {
+                        this.score = tempScore;
+                    }
+                    else if(tempScore.get(FOURTH_CARD_POSITION) >= fourthCardScore) {
+                        if (tempScore.get(FOURTH_CARD_POSITION) > fourthCardScore) {
+                            this.score = tempScore;
+                        }
+                        else if(tempScore.get(FIFTH_CARD_POSITION) >= fifthCardScore) {
+                            if (tempScore.get(FIFTH_CARD_POSITION) > fifthCardScore) {
+                                this.score = tempScore;
+                            }
                         }
                     }
                 }
@@ -93,113 +124,120 @@ public class ScoreUpdate implements Serializable {
      * @param playerHandScores the scores of the cards in a hand.
      */
     private void setInitialHand(ArrayList<Integer> playerHandScores) {
-        for (int i = 0; i < PLAYER_HAND_SIZE; i++) {
-            playerHandScores.add(0);
+        for (int i = FIRST_CARD_POS; i < PLAYER_HAND_SIZE; i++) {
+            addCardRankToRanks(playerHandScores, FIRST_CARD_POS);
         }
     }
 
-//todo shorten this method / split it into submethods, and also decrease number of parameters if possible
     /**
      * Evaluate the score of a player's hand.
-     * @param card1
-     * @param card2
-     * @param card3
-     * @param card4
-     * @param card5
-     * @return
+     * @param card1 the first card on hand.
+     * @param card2 the second card on hand.
+     * @param card3 the third card on hand.
+     * @param card4 the fourth card on hand.
+     * @param card5 the fifth card on hand.
+     * @return the hand score.
      */
     public ArrayList<Integer> evaluate(Card card1, Card card2, Card card3, Card card4, Card card5){
         ArrayList<Integer> ranks = new ArrayList<>();
-        ranks.add(card1.getRank()); //todo avoid duplicated code
-        ranks.add(card2.getRank());
-        ranks.add(card3.getRank());
-        ranks.add(card4.getRank());
-        ranks.add(card5.getRank());
-
         ArrayList<Integer> suits = new ArrayList<>();
-        suits.add(card1.getSuit());
-        suits.add(card2.getSuit());
-        suits.add(card3.getSuit());
-        suits.add(card4.getSuit());
-        suits.add(card5.getSuit());
+        ArrayList<Card> cardsOnHand = new ArrayList<>(PLAYER_HAND_SIZE);
 
-        ArrayList<Integer> tempScore = new ArrayList<>(5);
+        cardsOnHand.add(card1);
+        cardsOnHand.add(card2);
+        cardsOnHand.add(card3);
+        cardsOnHand.add(card4);
+        cardsOnHand.add(card5);
+
+        for (Card card : cardsOnHand) {
+            addCardRankToRanks(ranks, card.getRank());
+            addCardSuitToSuits(ranks, card.getSuit());
+        }
+
+        ArrayList<Integer> tempScore = new ArrayList<>(PLAYER_HAND_SIZE);
         setInitialHand(tempScore);
 
         if (flush(ranks, suits) != 0 && straight(ranks) != 0){
-            tempScore.set(0,flush(ranks, suits) + 300);
-        }
-        else if (kind(ranks) > 800){ //todo avoid magic numbers
-            tempScore.set(0,kind(ranks));
-            ranks.remove(tempScore.get(0) - 800);
-            ranks.remove(tempScore.get(0) - 800);
-            ranks.remove(tempScore.get(0) - 800);
-            ranks.remove(tempScore.get(0) - 800);
-            tempScore.set(1,getMax(ranks));
-        }
-        else if (fullHouse(ranks) != 0) {
-            tempScore.set(0,fullHouse(ranks));
-            ranks.remove(tempScore.get(0) - 700);
-            ranks.remove(tempScore.get(0) - 700);
-            ranks.remove(tempScore.get(0) - 700);
-            tempScore.set(1,getMax(ranks));
-        }
-        else if (flush(ranks,suits) != 0){
-            tempScore.set(0,flush(ranks,suits));
-            ranks.remove(tempScore.get(0) - 600);
-            tempScore.set(1,getMax(ranks));
-            ranks.remove(tempScore.get(1));
-            tempScore.set(2,getMax(ranks));
-            ranks.remove(tempScore.get(2));
-            tempScore.set(3,getMax(ranks));
-            ranks.remove(tempScore.get(3));
-            tempScore.set(4,getMax(ranks));
-        }
-        else if (straight(ranks) != 0) {
-            tempScore.set(0,straight(ranks));
-        }
-        else if (kind(ranks) > 400){
-            tempScore.set(0,kind(ranks));
-            ranks.remove(tempScore.get(0) - 400);
-            ranks.remove(tempScore.get(0) - 400);
-            ranks.remove(tempScore.get(0) - 400);
-            tempScore.set(1,getMax(ranks));
-            ranks.remove(tempScore.get(1));
-            tempScore.set(2,getMax(ranks));
-        }
-        else if (twoPair(ranks) != 0){
-            tempScore.set(0,twoPair(ranks));
-            ranks.remove(tempScore.get(0) - 300);
-            ranks.remove(tempScore.get(0) - 300);
-            tempScore.set(1,kind(ranks)-200);
-            ranks.remove(tempScore.get(1));
-            ranks.remove(tempScore.get(1));
-            tempScore.set(2,getMax(ranks));
-        }
-        else if (kind(ranks) > 200){
-            tempScore.set(0,kind(ranks));
-            ranks.remove(tempScore.get(0) - 200);
-            ranks.remove(tempScore.get(0) - 200);
-            tempScore.set(1,getMax(ranks));
-            ranks.remove(tempScore.get(1));
-            tempScore.set(2,getMax(ranks));
-            ranks.remove(tempScore.get(2));
-            tempScore.set(3,getMax(ranks));
+            tempScore.set(FIRST_CARD_POS,flush(ranks, suits) + SCORE_300);
         }
         else {
-            tempScore.set(0,kind(ranks));
-            ranks.remove(tempScore.get(0) - 100);
-            tempScore.set(1,getMax(ranks));
-            ranks.remove(tempScore.get(1));
-            tempScore.set(2,getMax(ranks));
-            ranks.remove(tempScore.get(2));
-            tempScore.set(3,getMax(ranks));
-            ranks.remove(tempScore.get(3));
-            tempScore.set(4,getMax(ranks));
+            if (kind(ranks) > SCORE_800){
+                tempScore.set(FIRST_CARD_POS,kind(ranks));
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_800);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_800);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_800);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_800);
+                tempScore.set(SECOND_CARD_POSITION,getMax(ranks));
+            }
+            else if (fullHouse(ranks) != 0) {
+                tempScore.set(FIRST_CARD_POS,fullHouse(ranks));
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_700);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_700);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_700);
+                tempScore.set(SECOND_CARD_POSITION,getMax(ranks));
+            }
+            else if (flush(ranks,suits) != 0){
+                tempScore.set(FIRST_CARD_POS,flush(ranks,suits));
+                setTempScore(ranks, tempScore, SCORE_600);
+            }
+            else if (straight(ranks) != FIRST_CARD_POS) {
+                tempScore.set(FIRST_CARD_POS,straight(ranks));
+            }
+            else if (kind(ranks) > SCORE_400){
+                tempScore.set(FIRST_CARD_POS,kind(ranks));
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_400);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_400);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_400);
+                tempScore.set(SECOND_CARD_POSITION,getMax(ranks));
+                ranks.remove(tempScore.get(SECOND_CARD_POSITION));
+                tempScore.set(THIRD_CARD_POSITION,getMax(ranks));
+            }
+            else if (twoPair(ranks) != FIRST_CARD_POS){
+                tempScore.set(FIRST_CARD_POS,twoPair(ranks));
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_300);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_300);
+                tempScore.set(SECOND_CARD_POSITION,kind(ranks)- SCORE_200);
+                ranks.remove(tempScore.get(SECOND_CARD_POSITION));
+                ranks.remove(tempScore.get(SECOND_CARD_POSITION));
+                tempScore.set(THIRD_CARD_POSITION,getMax(ranks));
+            }
+            else if (kind(ranks) > SCORE_200){
+                tempScore.set(FIRST_CARD_POS,kind(ranks));
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_200);
+                ranks.remove(tempScore.get(FIRST_CARD_POS) - SCORE_200);
+                tempScore.set(SECOND_CARD_POSITION,getMax(ranks));
+                ranks.remove(tempScore.get(SECOND_CARD_POSITION));
+                tempScore.set(THIRD_CARD_POSITION,getMax(ranks));
+                ranks.remove(tempScore.get(THIRD_CARD_POSITION));
+                tempScore.set(FOURTH_CARD_POSITION,getMax(ranks));
+            }
+            else {
+                tempScore.set(FIRST_CARD_POS,kind(ranks));
+                setTempScore(ranks, tempScore, SCORE_100);
+            }
         }
 
 
         return tempScore;
+    }
+
+    private void setTempScore(ArrayList<Integer> ranks, ArrayList<Integer> tempScore, int score_600) {
+        ranks.remove(tempScore.get(FIRST_CARD_POS) - score_600);
+        tempScore.set(SECOND_CARD_POSITION,getMax(ranks));
+        ranks.remove(tempScore.get(SECOND_CARD_POSITION));
+        tempScore.set(THIRD_CARD_POSITION,getMax(ranks));
+        ranks.remove(tempScore.get(THIRD_CARD_POSITION));
+        tempScore.set(FOURTH_CARD_POSITION,getMax(ranks));
+        ranks.remove(tempScore.get(FOURTH_CARD_POSITION));
+        tempScore.set(FIFTH_CARD_POSITION,getMax(ranks));
+    }
+
+    private void addCardSuitToSuits(ArrayList<Integer> suits, int suit) {
+        suits.add(suit);
+    }
+
+    private void addCardRankToRanks(ArrayList<Integer> ranks, int cardRank) {
+        ranks.add(cardRank);
     }
 
     public ArrayList<Integer> getScore() {
@@ -214,23 +252,23 @@ public class ScoreUpdate implements Serializable {
     public int kind(ArrayList<Integer> ranks){
         int kindCount = 0;
         int rank = 0;
-        for(int i = 0; i < ranks.size(); i++){
+        for(int i = FIRST_CARD_POS; i < ranks.size(); i++){
             if (countMatches(ranks, ranks.get(i)) > kindCount){
                 kindCount = countMatches(ranks, ranks.get(i));
                 rank = ranks.get(i);
             }
         }
         if (kindCount == 4){
-            return 800 + rank;
+            return SCORE_800 + rank;
         }
         else if (kindCount == 3){
-            return 400 + rank;
+            return SCORE_400 + rank;
         }
         else if (kindCount == 2){
-            return 200 + rank;
+            return SCORE_200 + rank;
         }
         else {
-            return 100+getMax(ranks);
+            return SCORE_100 + getMax(ranks);
         }
     }
 
@@ -242,12 +280,12 @@ public class ScoreUpdate implements Serializable {
     public int fullHouse(ArrayList<Integer> ranks){
         int count = 0;
         ArrayList<Integer> ranks1 = new ArrayList<>(ranks);
-        if (500 > kind(ranks) && kind(ranks) > 400){
-            ranks1.remove(kind(ranks) - 400);
-            ranks1.remove(kind(ranks) - 400);
-            ranks1.remove(kind(ranks) - 400);
-            if (kind(ranks1) > 200){
-                count = kind(ranks) + 300;
+        if (SCORE_500 > kind(ranks) && kind(ranks) > SCORE_400){
+            ranks1.remove(kind(ranks) - SCORE_400);
+            ranks1.remove(kind(ranks) - SCORE_400);
+            ranks1.remove(kind(ranks) - SCORE_400);
+            if (kind(ranks1) > SCORE_200){
+                count = kind(ranks) + SCORE_300;
             }
         }
         return count;
@@ -261,15 +299,15 @@ public class ScoreUpdate implements Serializable {
     public int twoPair(ArrayList<Integer> ranks){
         int count = 0;
         ArrayList<Integer> ranks1 = new ArrayList<>(ranks);
-        if (300 > kind(ranks) && kind(ranks) > 200){
-            ranks1.remove(kind(ranks) - 200);
-            ranks1.remove(kind(ranks) - 200);
+        if (SCORE_300 > kind(ranks) && kind(ranks) > SCORE_200){
+            ranks1.remove(kind(ranks) - SCORE_200);
+            ranks1.remove(kind(ranks) - SCORE_200);
 
-            if (kind(ranks1) > 200){
+            if (kind(ranks1) > SCORE_200){
                 ArrayList<Integer> numbers = new ArrayList<>();
                 numbers.add(kind(ranks));
                 numbers.add(kind(ranks1));
-                count = getMax(numbers) + 100;
+                count = getMax(numbers) + SCORE_100;
             }
         }
         return count;
@@ -282,12 +320,12 @@ public class ScoreUpdate implements Serializable {
      * @return the count.
      */
     public int flush(ArrayList<Integer> ranks, ArrayList<Integer> suits) {
-        for (int i = 1; i < suits.size(); i++){
-            if (!suits.get(0).equals(suits.get(i))){
+        for (int i = SECOND_CARD_POSITION; i < suits.size(); i++){
+            if (!suits.get(FIRST_CARD_POS).equals(suits.get(i))){
                 return 0;
             }
         }
-        return 600 + getMax(ranks);
+        return SCORE_600 + getMax(ranks);
     }
 
     /**
@@ -297,12 +335,12 @@ public class ScoreUpdate implements Serializable {
      */
     public int straight(ArrayList<Integer> ranks){
         Collections.sort(ranks);
-        for (int i = 0; i < ranks.size()-1; i++) {
-            if (ranks.get(i) + 1 != ranks.get(i+1)){
+        for (int i = FIRST_CARD_POS; i < ranks.size() - 1; i++) {
+            if (ranks.get(i) + 1 != ranks.get(i + 1)){
                 return 0;
             }
         }
-        return 500 + getMax(ranks);
+        return SCORE_500 + getMax(ranks);
     }
 
     /**
@@ -311,8 +349,8 @@ public class ScoreUpdate implements Serializable {
      * @return the highest value.
      */
     public int getMax(ArrayList<Integer> values){
-        int max = values.get(0);
-        for (int i = 1; i < values.size(); i++){
+        int max = values.get(FIRST_CARD_POS);
+        for (int i = SECOND_CARD_POSITION; i < values.size(); i++){
             if (values.get(i) > max){
                 max = values.get(i);
             }
